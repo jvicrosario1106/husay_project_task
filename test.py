abbe9a817ce7ab666871a240b73899bac7c8b6b4
JV Rosario <jvrosario@JVs-MacBook-Pro.local>
2022-04-21 08:23:40 +0800

    updated

--- test.py
+++ test.py
@@ -1,6 +1,32 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup as bs
+import requests
 ser = Service("driver/chromedriver")
 op = webdriver.ChromeOptions()
 s = webdriver.Chrome(service=ser, options=op)
 s.get("https://www.google.com")
+
+job_list = []
+
+headers = {
+'authority': 'scrapeme.live',
+'dnt': '1',
+'upgrade-insecure-requests': '1',
+'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
+'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+'sec-fetch-site': 'none',
+'sec-fetch-mode': 'navigate',
+'sec-fetch-user': '?1',
+'sec-fetch-dest': 'document',
+'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+}
+
+def generate_url(job_title,page):
+    url_template = "https://www.jobstreet.com.ph/en/job-search/{}-jobs/{}/"
+    format_url = url_template.format(job_title,page)
+    return format_url
+
+for i in range(1, 3):
+    url = generate_url("Multimedia Artist", "1") # Change Job title and pages
+    s.get(url)
