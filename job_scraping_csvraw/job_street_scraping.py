64651c8e4ec660afc9fa57642d2832903f8d5442
JV Rosario <jvrosario@JVs-MacBook-Pro.local>
2022-04-26 14:18:48 +0800

    updated

--- job_scraping_csvraw/job_street_scraping.py
+++ job_scraping_csvraw/job_street_scraping.py
@@ -15,7 +15,7 @@ s.maximize_window()
 
 df = pd.DataFrame(columns=["Title","Company","Location","Summary","Salary","Qualification & Skills","Role and Responsibilities","Job Type","Company Profile&Job Desc", "Career Level", "Benefits","Company Size"])
 
-job = "creative director"
+job = "sound-engineer"
 
 for page in range(2,10):
     url = "https://www.jobstreet.com.ph/en/job-search/{}-jobs/{}/".format(job,page)
@@ -27,11 +27,6 @@ for page in range(2,10):
     number_of_cards = html_job_lists.find_elements(By.TAG_NAME, "article")
  
     for card in range(0,len(number_of_cards)):
-        
-        responsbilities_append = []
-        qualifications_append = []
-        
-
         job_card = WebDriverWait(s,30).until(EC.element_to_be_clickable((By.XPATH, "//article[@data-automation='job-card-{}']".format(card))))
         try:
             job_card.click()
@@ -40,13 +35,14 @@ for page in range(2,10):
             job_card.click()
 
         while s.current_url != url:
-            time.sleep(2)
-            s.execute_script("window.history.go(-1)")
+            time.sleep(3)
+            #s.get("https://www.jobstreet.com.ph/en/job-search/{}-jobs/{}/".format(job,page))
+            s.back()
+            #s.execute_script("window.history.go(-1)")
 
         description = WebDriverWait(s,30).until(EC.presence_of_element_located((By.XPATH, "//div[@data-automation='jobDescription']")))
         if(description):
             
-
             job_descriptions = s.find_element(By.XPATH, "//div[@data-automation='jobDescription']")
             soup_one = bs(job_descriptions.get_attribute("innerHTML"), "html.parser")
             job_details = s.find_element(By.XPATH, "//div[@data-automation='detailsTitle']")  
@@ -57,7 +53,7 @@ for page in range(2,10):
             
             company_location = soup_three.find_all("span", class_="sx2jih0 zcydq84u _18qlyvc0 _18qlyvc1x _18qlyvc1 _18qlyvca")
             
-            # -------- EXTRACTING DATA ----------# 
+            # -------- EXTRACTING DATA ---------- # 
             job_title = soup_two.find("h1").text
             company_name =soup_two.find("span").text
             location = company_location[0].text
@@ -75,7 +71,6 @@ for page in range(2,10):
             qualification = soup_one.find_all("li")
             responsibilities = soup_one.find_all("p")
 
-        
             additional_info = s.find_element(By.XPATH, "//div[@class='sx2jih0 zcydq86q zcydq86v zcydq86w zcydq87q zcydq87v zcydq87w zcydq896 zcydq886 _18qlyvc14 _18qlyvc17 zcydq832 zcydq835']")
             soup_four = bs(additional_info.get_attribute("innerHTML"),"html.parser")
             career_level = soup_four.find_all(text=re.compile("Experienced"))
@@ -83,8 +78,7 @@ for page in range(2,10):
             company_size = soup_four.find_all(text=re.compile("Employees"))
             job_type =  soup_four.find_all(text=re.compile("Time"))
             company_overview = soup_four.find_all("div",class_="YCeva_0")
-
-              
+  
             for cs in company_size:
                 if "-" not in cs:
                     size = "None"
@@ -99,6 +93,6 @@ for page in range(2,10):
 
             df = df.append({"Title":job_title,"Company":company_name,"Location":location,"Summary":summary,"Salary":salary,"Qualification & Skills":[q.text for q in qualification],"Role and Responsibilities":[r.text for r in responsibilities],"Job Type":job_type,"Company Profile&Job Desc":[co.find("div").text for co in company_overview], "Career Level":career, "Benefits":benefits,"Company Size":size},ignore_index=True)
  
+    df.to_csv("Jobstreet-{}.csv".format(job),index=False)
 
-df.to_csv("Jobstreet-{}.csv".format(job),index=False)
 s.close()    
